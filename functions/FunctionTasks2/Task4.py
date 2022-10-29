4b334290f54eec8e9896d0b24439def8c461f880
Настя Дорощук <113581587+naannad@users.noreply.github.com>
2022-10-29 11:46:36 +0200

    Add files via upload

---
+++ functions/FunctionTasks2/Task4.py
@@ -0,0 +1,23 @@
+"""
+Напишите функцию cacluate, которая может принимать любое количество чисел и возвращать кортеж.
+
+Первое значение кортежа - это среднее значение всех параметров, а второе значение - все числа, превышающие среднее значение.
+Второе значение оформите в виде списка.
+"""
+def cacluate(*numbers):
+    counter = 0
+
+    t = ()
+    num = []
+    for i in numbers:
+        counter = counter + i
+    formul = counter / len(numbers)
+
+    for i in numbers:
+        if i > formul:
+            num.append(i)
+
+    t = (formul, num)
+    return t
+
+print(cacluate(7,7,5,4,8,9,2,1,3,4,5))
