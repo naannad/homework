19170e8265a0eb7ce7e81503f7ab3dc6ce79ec52
Настя Дорощук <113581587+naannad@users.noreply.github.com>
2023-05-09 16:37:32 +0300

    update task2.py

--- Selenium/Task2.py
+++ Selenium/Task2.py
@@ -2,3 +2,20 @@
 Напишите программу которая автоматически собирает ваше расписание в Элжуре. и сохраняет в json файл в виде:
 {день недели: {Предмет: Аудитория}
 """
+import json
+
+from selenium import webdriver
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.webdriver.firefox.options import Options
+
+binary = FirefoxBinary('/home/sirius/.mozilla/firefox/gwi6uqpe.Default')
+options = Options()
+driver = webdriver.Firefox(firefox_binary=binary)
+
+
+url = 'https://class.sirius.ru/authorize'
+driver.get(url)
+
+content = driver.find_element(by='inputiyfemonOI-S').text
+parsed_json = json.loads(content)
+print(parsed_json)
